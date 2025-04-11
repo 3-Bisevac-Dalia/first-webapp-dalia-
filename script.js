8fbc749b84b829d666bc3f530813f548babd43b8
3-Bisevac-Dalia <dalia.bisevac@gmail.com>
2025-04-11 09:23:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Add JavaScript code for your web site here and call it from index.html.
+document.getElementById("myBtn")?.addEventListener("click",() => {
+    let input = document.getElementById("myInput").value;
+    document.getElementById("myPara").innerHTML = " Hello  " + input + "! Explore this page to learn all about  Matel's old Barbie Movies and their rankings!";
+ })
+ 
