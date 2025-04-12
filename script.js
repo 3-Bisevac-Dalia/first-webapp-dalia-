1ece83c00905ca5ef38dd4488d565b67bcca83f8
3-Bisevac-Dalia <dalia.bisevac@gmail.com>
2025-04-11 18:30:48 -0700

    script.js
    Update

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ document.getElementById("myBtn")?.addEventListener("click",() => {
     let input = document.getElementById("myInput").value;
     document.getElementById("myPara").innerHTML = " Hello  " + input + "! Explore this page to learn all about  Matel's old Barbie Movies and their rankings!";
  })
- 
+
+ document.getElementById("submitMovieBtn")?.addEventListener("click", function () {
+    let favoriteMovie = document.getElementById("favoriteMovie").value;
+    document.getElementById("responseMessage").innerHTML = favoriteMovie + ", that's a great choice! Remember, every movie has some of that Barbie magic, so the only ranking that really matters is the one in your heart!";
+    document.getElementById("favoriteMovie").value = "";
+ });
